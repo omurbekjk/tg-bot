52045b745936dfeb6270a0ef2b923201e1c4364d
omurbekjk <omurbek.jk@gmail.com>
2020-01-21 11:37:06 +0600

    OK telebot package added and simple bot configs

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
 
 func main() {
-	token := "949040307:AAF1czN0rfQKBP7-VY9in7PWBpQJ1qo-mfw222"
-	fmt.Print(token)
+	var (
+		port      = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
+		token     = os.Getenv("TOKEN")      // you must add it to your config vars
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hi!")
+	})
+
+	b.Handle("/hi", func(m *tb.Message) {
+		b.Send(m.Sender, "You entered "+m.Text)
+	})
+
+	inlineBtn1 := tb.InlineButton{
+		Unique: "moon",
+		Text:   "Moon 🌚",
+	}
+
+	inlineBtn2 := tb.InlineButton{
+		Unique: "sun",
+		Text:   "Sun 🌞",
+	}
+
+	b.Handle(&inlineBtn1, func(c *tb.Callback) {
+		// Required for proper work
+		b.Respond(c, &tb.CallbackResponse{
+			ShowAlert: false,
+		})
+		// Send messages here
+		b.Send(c.Sender, "Moon says 'Hi'!")
+	})
+
+	b.Handle(&inlineBtn2, func(c *tb.Callback) {
+		b.Respond(c, &tb.CallbackResponse{
+			ShowAlert: false,
+		})
+		b.Send(c.Sender, "Sun says 'Hi'!")
+	})
+
+	inlineKeys := [][]tb.InlineButton{
+		[]tb.InlineButton{inlineBtn1, inlineBtn2},
+	}
+
+	b.Handle("/pick_time", func(m *tb.Message) {
+		b.Send(
+			m.Sender,
+			"Day or night, you choose",
+			&tb.ReplyMarkup{InlineKeyboard: inlineKeys})
+	})
+
+	b.Start()
 }
